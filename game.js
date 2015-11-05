8b94dbc2e861221471d78bacc720a8bdc8b59a46
meeksfred <maxfred0025@gmail.com>
2015-11-05 14:39:12 -0800

    turn questions into functions, housekeeping

--- game.js
+++ game.js
@@ -5,17 +5,30 @@ alert("Very nice to meet you, " + name + ", and welcome to Max's Guessing Game!"
 
 // A nice greeting for those coming to play my game.
 
-var userResponse1 = prompt("Was I born in the state of Iowa? (yes or no)").toLowerCase();
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+
+// Calling all my functions here
+
+function questionOne() {
+  var userResponse1 = prompt("Was I born in the state of Iowa? (yes or no)").toLowerCase();
   if ((userResponse1 === "n") || (userResponse1 === "no")) {
-    console.log("User was correct");
+    console.log("User is correct");
     alert("You are right! I was actually born in the state of Washington.");
     finalScore++;
   } else {
-    console.log("User was incorrect");
+    console.log("User is incorrect");
     alert("Incorrect. I was born in Washington, but most of my family does live in Iowa.");
-  };
+  }
+};
 
-var userResponse2 = prompt("Is baseball my favorite sport? (yes or no)").toLowerCase();
+
+function questionTwo() {
+  var userResponse2 = prompt("Is baseball my favorite sport? (yes or no)").toLowerCase();
   if ((userResponse2 === "y") || (userResponse2 === "yes")) {
     console.log("User is correct");
     alert("Correct! America's past time");
@@ -23,9 +36,12 @@ var userResponse2 = prompt("Is baseball my favorite sport? (yes or no)").toLower
   } else {
     console.log("User is incorrect");
     alert("Incorrect. It actually is baseball, however I do love all sports");
-  };
+  }
+};
 
-var userResponse3 = prompt("Is pizza my favorite food? (yes or no)").toLowerCase();
+
+function questionThree() {
+  var userResponse3 = prompt("Is pizza my favorite food? (yes or no)").toLowerCase();
   if ((userResponse3 === "y") || (userResponse3 === "yes")) {
     console.log("User is correct");
     alert("Of course! Pizza is the greatest!");
@@ -33,33 +49,41 @@ var userResponse3 = prompt("Is pizza my favorite food? (yes or no)").toLowerCase
   } else {
     console.log("User is incorrect");
     alert("No silly, Pizza is absolutely the greatest food.");
-  };
-
-var x = 7
-var chances = 3
-var numHint = 'Now its time to guess my favorite number. Go ahead, any number between 1-50.'
-
-while (chances > 0) {
-  var guess = prompt(numHint + ' You have ' + chances + ' guesses remaining.');
-  if (!guess) break;
-  guess = Number(guess);
-  if (guess === x) {
-    alert('Great Job! ' + x + ' is indeed my favorite number.');
-    chances = 0;
-    finalScore++;
-}   else {
-    numHint = 'Incorrect.';
-    if (guess > x)
-      numHint += ' Too large! Try again.';
-    if (guess < x)
-      numHint += ' Too small! Try again.';
-    chances = chances - 1;
-    if (chances === 0)
-      alert("My number was actually 7. Good try!");
   }
 };
 
-var userResponse4 = prompt('Is Appa the coolest flying bison in existence? (yes or no)').toLowerCase();
+
+function questionFour() {
+  var x = 7
+  var chances = 3
+  var numHint = 'Now its time to guess my favorite number. Go ahead, any number between 1-50.'
+
+  while (chances > 0) {
+    var guess = prompt(numHint + ' You have ' + chances + ' guesses remaining.');
+    if (!guess) break;
+    guess = Number(guess);
+    if (guess === x) {
+      alert('Great Job! ' + x + ' is indeed my favorite number.');
+      console.log('User is correct.')
+      chances = 0;
+      finalScore++;
+  } else {
+      numHint = 'Incorrect.';
+      if (guess > x)
+        numHint += ' Too large! Try again.';
+      if (guess < x)
+        numHint += ' Too small! Try again.';
+      chances = chances - 1;
+      if (chances === 0)
+        alert("My number was actually 7. Good try!");
+        console.log('User is incorrect.');
+    }
+  }
+};
+
+
+function questionFive() {
+  var userResponse4 = prompt('Is Appa the coolest flying bison in existence? (yes or no)').toLowerCase();
   if ((userResponse4 === 'yes') || (userResponse4 === 'y')) {
     console.log('User is correct.');
     alert('Correct! Appa is the coolest, and only flying bison in existence');
@@ -67,9 +91,12 @@ var userResponse4 = prompt('Is Appa the coolest flying bison in existence? (yes 
   } else {
     console.log('User is incorrect.');
     alert('Incorrect. Appa is absolutely the coolest flying bison in the world.');
-  };
+  }
+};
 
-var userResponse5 = Number(prompt('How many licks does it officially take to get to the center of a Tootsie Pop? (enter a number)'));
+
+function questionSix() {
+  var userResponse5 = Number(prompt('How many licks does it officially take to get to the center of a Tootsie Pop? (enter a number)'));
   if (userResponse5 === 364) {
     console.log('User is correct.');
     alert('Correct! Holy cow, great guessing.');
@@ -77,7 +104,8 @@ var userResponse5 = Number(prompt('How many licks does it officially take to get
   } else {
     console.log('User is incorrect.');
     alert('Incorrect. The official unofficial number is 364. You were ' + (364 - Number(userResponse5)) + ' licks off.');
-  };
+  }
+};
 
 
 alert("Congratulations! Your score was " + finalScore + " out of 6.")
