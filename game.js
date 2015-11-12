dc4219cad0f4a85e1f0b40d106854a471f34f89a
meeksfred <maxfred0025@gmail.com>
2015-11-12 12:10:05 -0800

    move questions and answer into arrays

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ alert("Very nice to meet you, " + name + ", and welcome to Max's Guessing Game!"
 
 // A nice greeting for those coming to play my game.
 
+var questions = ['Was I born in the state of Iowa? (yes or no)', 'Is baseball my favorite sport? (yes or no)', 'Is pizza my favorite food? (yes or no)', 'Now its time to guess my favorite number. Go ahead, any number between 1-50.', 'Is Appa the coolest flying bison in existence? (yes or no)', 'How many licks does it officially take to get to the center of a Tootsie Pop? (enter a number)'];
+
+var answers = ['n', 'no', 'y', 'yes', 'y', 'yes', 7, 'y', 'yes', 364];
+
 questionOne();
 questionTwo();
 questionThree();
@@ -14,8 +18,8 @@ questionSix();
 // Calling all my functions here
 
 function questionOne() {
-  var userResponse1 = prompt("Was I born in the state of Iowa? (yes or no)").toLowerCase();
-  if ((userResponse1 === "n") || (userResponse1 === "no")) {
+  var userResponse1 = prompt(questions[0]).toLowerCase();
+  if ((userResponse1 === answers[0]) || (userResponse1 === answers[1])) {
     console.log("User is correct");
     finalScore++;
     document.getElementById('answerOne').innerHTML = "You are right! I was actually born in the state of Washington.";
@@ -31,8 +35,8 @@ function questionOne() {
 
 
 function questionTwo() {
-  var userResponse2 = prompt("Is baseball my favorite sport? (yes or no)").toLowerCase();
-  if ((userResponse2 === "y") || (userResponse2 === "yes")) {
+  var userResponse2 = prompt(questions[1]).toLowerCase();
+  if ((userResponse2 === answers[2]) || (userResponse2 === answers[3])) {
     console.log("User is correct");
     finalScore++;
     document.getElementById('answerTwo').innerHTML = "Correct! America's and Max's past time.";
@@ -48,8 +52,8 @@ function questionTwo() {
 
 
 function questionThree() {
-  var userResponse3 = prompt("Is pizza my favorite food? (yes or no)").toLowerCase();
-  if ((userResponse3 === "y") || (userResponse3 === "yes")) {
+  var userResponse3 = prompt(questions[2]).toLowerCase();
+  if ((userResponse3 === answers[4]) || (userResponse3 === answers[5])) {
     console.log("User is correct");
     finalScore++;
     document.getElementById('answerThree').innerHTML = "Of course! Pizza is the greatest!";
@@ -65,9 +69,9 @@ function questionThree() {
 
 
 function questionFour() {
-  var x = 7
+  var x = answers[6]
   var chances = 3
-  var numHint = 'Now its time to guess my favorite number. Go ahead, any number between 1-50.'
+  var numHint = questions[3]
 
   while (chances > 0) {
     var guess = prompt(numHint + ' You have ' + chances + ' guesses remaining.');
@@ -103,8 +107,8 @@ function questionFour() {
 
 
 function questionFive() {
-  var userResponse4 = prompt('Is Appa the coolest flying bison in existence? (yes or no)').toLowerCase();
-  if ((userResponse4 === 'yes') || (userResponse4 === 'y')) {
+  var userResponse4 = prompt(questions[4]).toLowerCase();
+  if ((userResponse4 === answers[7]) || (userResponse4 === answers[8])) {
     console.log('User is correct.');
     finalScore++;
     document.getElementById('answerFive').innerHTML = "Correct! Appa is the coolest, and only flying bison in existence.";
@@ -120,8 +124,8 @@ function questionFive() {
 
 
 function questionSix() {
-  var userResponse5 = Number(prompt('How many licks does it officially take to get to the center of a Tootsie Pop? (enter a number)'));
-  if (userResponse5 === 364) {
+  var userResponse5 = Number(prompt(questions[5]));
+  if (userResponse5 === answers[9]) {
     console.log('User is correct.');
     finalScore++;
     document.getElementById('answerSix').innerHTML = "Correct! Holy cow, great guessing.";
